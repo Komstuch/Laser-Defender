274a22174d598993e82930abe1cc38f8ac6a4969
Komstuch <komstuchxbox@gmail.com>
2018-12-25 13:33:24 +0100

    Extract Move method, change movemnt to 2D. Implement general input

--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 15.0f;
-	public float padding = 0.5f;
+	[SerializeField] float moveSpeed = 15.0f;
+    private float xMin, xMax;
+    public float padding = 0.5f;
 	public GameObject projectile;
 	public float projectileSpeed = 5f;
 	public float firingRate = 0.2f;
@@ -14,14 +16,23 @@ public class PlayerController : MonoBehaviour {
 	float xmin, xmax;
 	
 	void Start(){
-		float distance = transform.position.z - Camera.main.transform.position.z; // Distance between object and the camera in z direction
-		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance)); //Lower left corner of our playspace in world coordinates
-		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
-		xmin = leftMost.x + padding;
-		xmax = rightMost.x - padding;
+		//float distance = transform.position.z - Camera.main.transform.position.z; // Distance between object and the camera in z direction
+		//Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance)); //Lower left corner of our playspace in world coordinates
+		//Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		//xmin = leftMost.x + padding;
+		//xmax = rightMost.x - padding;
+        SetMoveboundaries();
+    
 	}
-	
-	void Fire(){
+
+    private void SetMoveboundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+
+    }
+
+    void Fire(){
 		Vector3 offset = new Vector3(0f, 0.5f, 0f);
 		GameObject laserBeam = Instantiate (projectile, transform.position+offset, Quaternion.identity)as GameObject;
 		laserBeam.GetComponent<Rigidbody2D>().velocity = new Vector3(0f, projectileSpeed, 0f);
@@ -37,15 +48,8 @@ public class PlayerController : MonoBehaviour {
 		if(Input.GetKeyUp(KeyCode.Space)){
 			CancelInvoke("Fire");
 		}
-		
-		if(Input.GetKey(KeyCode.LeftArrow)){
-			transform.position += Vector3.left * speed * Time.deltaTime;
-		} else if(Input.GetKey(KeyCode.RightArrow)){
-			transform.position += Vector3.right * speed * Time.deltaTime;
-		}	
-		// Restrict the player to the gamespace
-		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);	
-		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+
+        Move();		
 	}
 	
 	void OnTriggerEnter2D(Collider2D collider){
@@ -66,6 +70,17 @@ public class PlayerController : MonoBehaviour {
 		Destroy (gameObject);
 		
 	}
-	
-	
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal");
+        var deltaY = Input.GetAxis("Vertical");
+        var newXPos = transform.position.x + deltaX * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY * Time.deltaTime * moveSpeed;
+        transform.position = new Vector3(newXPos, newYPos, transform.position.z);
+
+        // Restrict the player to the gamespace
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+    }
+
 }
