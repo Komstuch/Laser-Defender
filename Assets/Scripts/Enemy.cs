0d27d6cfbf8a900be495cca81f2594306e3ba211
Komstuch <komstuchxbox@gmail.com>
2019-01-04 21:05:28 +0100

    Created Damage dealer & enemy class
    Add them to both pojectiles and enemy prefabs

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float health = 100;
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        health -= damageDealer.GetDamage();
+    }
+
+
+}
