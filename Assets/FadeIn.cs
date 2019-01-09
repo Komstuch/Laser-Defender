ee85c5e429a186c834a2bf7982bf6982062a6ba5
Komstuch <komstuchxbox@gmail.com>
2019-01-09 18:43:21 +0100

    Fade In at the beginning of the game

---
+++ Assets/FadeIn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+    [SerializeField] float fadeInTime = 2f;
+    float transparency;
+
+    void Start()
+    {
+        gameObject.SetActive(true);
+        transparency = this.GetComponent<CanvasRenderer>().GetAlpha();
+    }
+
+    void Update()
+    {
+        if (transparency > 0)
+        {
+            transparency -= Time.deltaTime/fadeInTime;
+            this.GetComponent<CanvasRenderer>().SetAlpha(transparency);
+        }
+        if (transparency <= 0 & gameObject.activeSelf == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
