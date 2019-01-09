7ae6c13952776885ccb781bbe9cfce103c3d79e2
Komstuch <komstuchxbox@gmail.com>
2019-01-09 19:55:07 +0100

    Improve fade panel

--- Assets/FadeIn.cs
+++ Assets/FadeIn.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeIn : MonoBehaviour
 {
     [SerializeField] float fadeInTime = 2f;
+
+    Image fadePanel;
+    Color currentColor;
     float transparency;
+
+    private void Awake()
+    {
+        gameObject.SetActive(true);
+    }
 
     void Start()
     {
-        gameObject.SetActive(true);
-        transparency = this.GetComponent<CanvasRenderer>().GetAlpha();
+        currentColor = new Color(0, 0, 0, 1f);
+        fadePanel = GetComponent<Image>();
+        fadePanel.color = currentColor;
+        transparency = fadePanel.color.a;
     }
 
     void Update()
     {
         if (transparency > 0)
         {
-            transparency -= Time.deltaTime/fadeInTime;
-            this.GetComponent<CanvasRenderer>().SetAlpha(transparency);
+            transparency -= Time.deltaTime / fadeInTime;
+            currentColor.a = transparency;
+            fadePanel.color = currentColor;            
         }
         if (transparency <= 0 & gameObject.activeSelf == true)
         {
