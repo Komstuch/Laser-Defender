939d37623c4a456a08733bd1bf29f58341e3c577
Komstuch <komstuchxbox@gmail.com>
2019-01-14 20:35:14 +0100

    Leaderboards functionlity works in console

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
+using GameSparks.Core;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
@@ -6,5 +9,38 @@ using UnityEngine.UI;
 
 public class Leaderboard : MonoBehaviour
 {
-   
+    int entryCount = 10;
+    int includeFirst = 10;
+    string leaderboardShortCode = "SCORE_LEADERBOARD";
+    string scoreKey = "SCORE";
+
+    private void Start()
+    {
+        GetLeaderboardFromGS();
+    }
+
+    private void GetLeaderboardFromGS()
+    {
+        Debug.Log("Getting leaderboard from GameSpark...");
+        new LeaderboardDataRequest()
+        .SetEntryCount(entryCount)
+        .SetIncludeFirst(includeFirst)
+        .SetLeaderboardShortCode(leaderboardShortCode)
+        .Send((response) => ProcessData(response));
+    }
+
+    private void ProcessData(LeaderboardDataResponse response)
+    {
+        string challengeInstanceId = response.ChallengeInstanceId;
+        GSEnumerable<LeaderboardDataResponse._LeaderboardData> data = response.Data;
+        string leaderboardShortCode = response.LeaderboardShortCode;
+        GSData scriptData = response.ScriptData;
+        foreach (LeaderboardDataResponse._LeaderboardData entry in data)
+        {
+            string score = entry.JSONData[scoreKey].ToString();
+            string name = entry.UserName.ToString();
+            Debug.Log("Player: " + name + ", Score: " + score);
+        }
+    }
 }
+
