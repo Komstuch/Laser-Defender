4659e013ae8e29246aec9c5e222c4aae93067094
Komstuch <komstuchxbox@gmail.com>
2019-04-10 20:24:09 +0200

    Add Link to the privacy policy in about menu

---
+++ Assets/Scripts/PrivacyPolicyLink.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrivacyPolicyLink : MonoBehaviour
+{
+    string link = "https://github.com/Komstuch/Laser-Defender/blob/PrivacyPolicy/privacy_policy.md";
+
+    public void OpenPolicy()
+    {
+        Application.OpenURL(link);
+    }
+}
